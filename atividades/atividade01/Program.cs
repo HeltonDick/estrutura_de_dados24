aff27dd3d33e00e4cb7d9ddaa1518d925234fc6a
Helton Gabriel Dick <427603@unoesc.lan>
2024-08-14 22:07:08 -0300

    Pirmeiro projeto .net

---
+++ atividades/atividade01/Program.cs
@@ -0,0 +1,19 @@
+﻿//Comentarios em c#
+
+/*
+    Comentario
+    de
+    multiplas
+    linhas
+*/
+
+//Declarando variavel
+int number;
+//Atribuindo valor
+number = 10;
+
+//Declarando const
+const int DAYS_IN_WEEK = 7;
+
+//Imprimindo string concatenada
+Console.WriteLine($"A semana tme {DAYS_IN_WEEK} dias.");
