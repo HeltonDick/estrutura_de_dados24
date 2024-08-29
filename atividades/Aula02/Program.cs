491e69c712a6e4e9027c18c867477f7786e8f18a
Helton Gabriel Dick <427603@unoesc.lan>
2024-08-28 21:33:35 -0300

    Aula 3 arrays e mais outras coisas git status clear

---
+++ atividades/Aula02/Program.cs
@@ -0,0 +1,22 @@
+﻿using Aula02;
+
+Person person = new Person();
+Console.WriteLine(person.Name);
+//Console.WriteLine(person.Age);
+
+Person person1 = new Person("Helton Dick", 19);
+Console.WriteLine(person1.Name);
+Console.WriteLine(person1.Age);
+
+Person person2 = new Person();
+person2.Name = "Pula vara";
+person2.Age = 120;
+Console.WriteLine(person2.Name);
+Console.WriteLine(person2.Age);
+
+Person person3 = new Person() {
+    Name = "Leonel Messi",
+    Age = 20
+};
+Console.WriteLine(person3.Name);
+Console.WriteLine(person3.Age);
--- atividades/atividade01/Program.cs
+++ atividades/atividade01/Program.cs
@@ -16,13 +16,13 @@ number = 10;
 const int DAYS_IN_WEEK = 7;
 
 //Imprimindo string concatenada
-Console.WriteLine($"A semana tme {DAYS_IN_WEEK} dias.");
+Console.WriteLine($"A semana tem {DAYS_IN_WEEK} dias.");
 
 //new TipoEnumerador() -> invoca o metodo construtor do objeto
 //Toda classe tem o seu construtor padrão implicito sem argumentos
 //É possivel sobreescrever este metodo especificando argumentos
 //Todavia, se o fizermos, perdemos o original implicito e 
 //Precissaremos defini-lo explicitamente
-TipoEnumerador tipoEnum = new TipoEnumerador();
-TipoEnumerador.Language enumEnglish = tipoEnum.GetLenguageEnum("english");
+Tipoenumerador tipoEnum = new Tipoenumerador();
+Tipoenumerador.Language enumEnglish = tipoEnum.GetLenguageEnum("english");
 Console.WriteLine($"O enum de english é {enumEnglish}");
