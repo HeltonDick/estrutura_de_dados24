80f8b5ea430d19f9023d3b6c28bd7a4aebd46e9b
Helton Gabriel Dick <427603@unoesc.lan>
2024-09-11 22:03:00 -0300

    Novo commit

---
+++ atividades/Pilhas/Program.cs
@@ -0,0 +1,22 @@
+﻿// As pilhas trabalham com uma estrutura LIFO -> Last In First Out
+/* Três metodos importantes:
+Push() - > Incere um elemento no topo da pilha
+Pop() - > Remove um elemento do topo e o retorna 
+Peek() - > Retorna o elemento do topo sem remove-lo
+*/
+
+// Invertendo as bola
+Stack<char> Caracteres = new Stack<char>();
+
+foreach (char c in "DARCY DA MONTANHA") {
+    Caracteres.Push(c);
+}
+
+// Desempilhando
+string invertido = string.Empty;
+while(Caracteres.Count > 0) {
+    invertido += Caracteres.Pop();
+}
+
+Console.WriteLine(invertido);
+
---
+++ atividades/PilhasAtividade/Program.cs
@@ -0,0 +1,9 @@
+﻿Stack<char> Caracteres = new Stack<char>();
+
+Console.WriteLine("Digite uma palavra e vamo ve see é palindromo: ");
+string ?palindromo = Console.ReadLine();
+string ?inverPalindro = 
+
+for (  int i = 0; i < palindromo.Length; i++) {
+
+}
