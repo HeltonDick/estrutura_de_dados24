2e936eed7649d4a6f038654512e97f5c72a68972
Helton Gabriel Dick <heltondick123@gmail.com>
2024-09-18 02:32:28 -0300

    Atividade array

--- atividades/atividade02 array/Program.cs
+++ atividades/atividade02 array/Program.cs
@@ -1,38 +1,24 @@
-﻿int[] pairs;
-int[] odds;
-int[] complete;
+﻿int[] pairs = new int[50];
+int[] odds = new int[50];
+int pairIndex = 0;
+int oddIndex = 0;
 
 for (int i = 0; i < 100; i++) {
     if (i %2 == 0) {
-        pairs[i] = i;
+        pairs[pairIndex] = i;
+        pairIndex++;
     }
     else {
-        odds[i] = i;
+        odds[oddIndex] = i;
+        oddIndex++;
     }
 
-    complete = pairs.Concat(odds).ToArray();
-
-    foreach (int number in complete) {
-    Console.WriteLine(number + " ");
-}
 }
 
 Console.WriteLine("---------------------");
 
-
-/*
-// Array de números ímpares
-int[] numerosImpares = Enumerable.Range(1, 100).Where(x => x % 2 != 0).ToArray();
-
-// Array de números pares
-int[] numerosPares = Enumerable.Range(1, 100).Where(x => x % 2 == 0).ToArray();
-
-// Concatenando as duas arrays em uma só
-int[] numerosCombinados = numerosImpares.Concat(numerosPares).ToArray();
-
-// Exibindo o resultado
-Console.WriteLine("Array combinada até 100:");
-foreach (int numero in numerosCombinados){
-    Console.Write(numero + " ");
+for (int i = 0; i < 50; i++) {
+    Console.WriteLine(pairs[i] + "\n" + odds[i]);
 }
-*/
+
+Console.WriteLine("---------------------");
