48451138639242c083c77acffb348d58a1de1a1c
Helton Gabriel Dick <427603@unoesc.lan>
2024-10-16 19:50:25 -0300

    algoritimos de ordenacao

---
+++ atividades/AlgoritimosCordenacao/BoubleSort/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using BubbleSort;
+
+int[] inteiros = {0, 12, 7, 1, 8};
+BubbleSort.BubbleSort.Print(inteiros);
+BubbleSort.BubbleSort.Sort<int>(inteiros);
+BubbleSort.BubbleSort.Print(inteiros);
+
+Console.WriteLine("Teste char");
+char[] chars= { 'x', 'z', 'b', 'c', 'a'};
+BubbleSort.BubbleSort.Print(chars);
+BubbleSort.BubbleSort.Sort(chars);
+BubbleSort.BubbleSort.Print(chars);
---
+++ atividades/AlgoritimosCordenacao/InsertionSort/Program.cs
@@ -0,0 +1,3 @@
+﻿using InsrtionSort;
+
+int[] numeros = {-11, 12, -42, 0, 1, 90, 68, 6, -9};
