877485dbf21e44801e4769543d2f9af32b36b2c7
Helton Gabriel Dick <427603@unoesc.lan>
2024-10-16 21:53:16 -0300

    Começando o trabalho

---
+++ atividades/AlgoritimosCordenacao/MergeSort/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine("Implementação do Merge Sort");
---
+++ atividades/AlgoritimosCordenacao/QuickSort/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Markup;
+
+Console.WriteLine("TESTANDO O QUICK SORT");
+int[] valores = new int[] {0, 15, 24, 1, 3, 6, 12, 9};
+QuickSort.QuickSort.Print<int>(valores);
+Console.WriteLine("Iniciando ordenação.");
+QuickSort.QuickSort.Sort<int>(valores, 0, valores.Length - 1);
+Console.WriteLine("Finzando orientação.");
+QuickSort.QuickSort.Print<int>(valores);
