cb1a4091fa88927766ef337f5f5d2a5d81940bdb
Helton Gabriel Dick <heltondick123@gmail.com>
2024-10-23 18:06:44 -0300

    MergeSort funcionando

--- atividades/AlgoritimosCordenacao/MergeSort/Program.cs
+++ atividades/AlgoritimosCordenacao/MergeSort/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Implementação do Merge Sort");
+﻿using System.Runtime.InteropServices;
+using MergeSort;
+
+Console.WriteLine("Implementação do Merge Sort");
+int[] inteiros = {5, 100, 72, 33, 10, 90, 23, 1};
+
+MergeSort.MergeSort.Print<int>(inteiros);
+Console.WriteLine("Iniciando ordenação");
+
+int[] organizados = MergeSort.MergeSort.Sort<int>(inteiros);
+Console.WriteLine("Ordenação completa");
+MergeSort.MergeSort.Print<int>(organizados);
