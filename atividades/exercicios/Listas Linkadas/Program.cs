8493db12f5fd4cf6a8d412e5fffcbcf36376a7fe
Helton Gabriel Dick <427603@unoesc.lan>
2024-11-27 20:26:57 -0300

    Ultimo dia de aula

---
+++ atividades/exercicios/Listas Linkadas/Program.cs
@@ -0,0 +1,10 @@
+﻿using Listas_Linkadas;
+
+Page pageFirst = new Page() {Content = "Nega do (...)"};
+Page pageSecond = new Page() {Content = "suvaco cabeludo (...)"};
+Page pageThird = new Page() {Content = "vem nimin (...)"};
+Page pageFourth = new Page() {Content = "vem que hoje (...)"};
+Page pageFifth = new Page() {Content = "eu quero (...)"};
+Page pageSixth = new Page() {Content = "faze (...)"};
+
+LinkedList<Page> pages
